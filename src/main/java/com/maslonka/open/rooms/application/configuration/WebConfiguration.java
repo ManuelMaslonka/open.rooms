073dccf0c6449f0a2dd3e2a764e53acb225d7a8c
Manuel Mašlonka <manuelmaslonka@icloud.com>
2025-09-06 16:32:08 +0200

    Add configuration files for development and native profiles, and update CORS settings

--- src/main/java/com/maslonka/open/rooms/application/configuration/WebConfiguration.java
+++ src/main/java/com/maslonka/open/rooms/application/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.maslonka.open.rooms.application.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${application.frontend.url:http://localhost:4200}")
+    private String frontendUrl;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("localhost:4200", "http://localhost:4200", "*", "**")
+                .allowedOrigins("localhost:4200", "http://localhost:4200", "*", "**", frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type");
     }
